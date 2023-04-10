5b7950d26e3598437fa67839b79f311cdd7ee6ce
aquibjaved25 <aquib.javed25@gmail.com>
2023-04-11 01:48:27 +0530

    first commit

---
+++ app/src/main/java/com/rxjava/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.rxjava
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.observers.DisposableObserver
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var myObserver: DisposableObserver<String>
+    private lateinit var myObservable: Observable<String>
+    private var compositeDisposable:CompositeDisposable  = CompositeDisposable()
+    private var greeting:String = "Hello from Rxjava"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        myObservable = Observable.just(greeting)
+
+        compositeDisposable.add(
+            myObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(getObserver())
+        )
+    }
+
+    private fun getObserver():DisposableObserver <String> {
+
+        myObserver = object : DisposableObserver<String> () {
+            override fun onNext(t: String) {
+                Log.d("RxJava", "onNext Invoked $t")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.d("RxJava", "onError Invoked")
+            }
+
+            override fun onComplete() {
+                Log.d("RxJava", "onComplete Invoked")
+            }
+
+
+        }
+
+         return myObserver
+    }
+}
